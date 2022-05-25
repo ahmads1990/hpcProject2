a872d75bb8cddf844a5bc8f33035610b455fff7b
ahmads1990 <ahmads20002000@gmail.com>
2022-05-25 05:46:06 +0200

    fun

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -51,7 +51,6 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 	return input;
 }
 
-
 void createImage(int* image, int width, int height, int index)
 {
 	System::Drawing::Bitmap MyNewImage(width, height);
@@ -77,197 +76,174 @@ void createImage(int* image, int width, int height, int index)
 	MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
 	cout << "result Image Saved " << index << endl;
 }
-static string ext = ".png";
-//this function should take n images from the folder and then put them in a dynamic array
-//the big array  is the number of the images and the small arrays is the size of each image 
-int** inputVideoframe(int n, int* w, int* h) //put the size of image in w & h
+static string ext = ".jpg";
+
+//this function returns M*n^2 array m number of images
+int** getImages(int n, int* w, int* h) //put the size of image in w & h
 {
 	int** image_array = new int* [n];
 	System::String^ imagePath;
 	string img, image_num, image;
-	int width = 0, hight = 0;
+	int width = 0, height = 0;
 	for (int i = 1; i <= n; i++)
 	{
-		string s;
-		if (i <= 9)
-		{
-			s = "00" + to_string(i);
-		}
-		else if (i <= 99)
-		{
-			s = "0" + to_string(i);
-		}
-		else
-		{
-			s = to_string(i);
-		}
-		image_num = s;
+		if (i <= 9) { image_num = "00" + to_string(i); }
+		else if (i <= 99) { image_num = "0" + to_string(i); }
+		else { image_num = to_string(i); }
 		image = image_num + ext;
-		img = "..//Data//Input//in000" + s;
-		string NewPath = img  + ".jpg";
-		imagePath = marshal_as<System::String^>(NewPath);
+		img = "..//Data//Input//in000" + image;
+
+		imagePath = marshal_as<System::String^>(img);
 		int* imageData = inputImage(&*w, &*h, imagePath);
 
 		image_array[i - 1] = new int[*w * *h];
 		image_array[i - 1] = imageData;
-
-
 	}
-
 	return image_array;
 }
+
 int main()
 {
 
-	MPI_Init(NULL, NULL);
-	//getchar();
-	int w = 0, h = 0;
-
+	int ImageWidth = 0, ImageHeight = 0;
 	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	int world_rank;
+	int ImageSize = 0;
+	int start_s, stop_s, TotalTime = 0;
+	// Initialize the MPI env
+	MPI_Init(NULL, NULL);
+	// Get processes number
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	// Get processor rank
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	int full_size_of_img = 0;
 
 
-	int** image_array = 0;
-	int* threshold_img = 0;
-	int imagenumber;
+	int** TotalImageArray = 0;
+	int* thresholdImage = 0;
+	int IMAGE_NUMBER;
 	int threshold;
 
 	if (world_rank == 0)
 	{
-		//to input the image 
-		imagenumber = 495;
-		threshold = 20;
-		image_array = inputVideoframe(imagenumber, &w, &h);
+		//assign data
+		IMAGE_NUMBER = 495;
+		//selected threshbhold
+		threshold = 50;
+		// Sequential part
+		TotalImageArray = getImages(IMAGE_NUMBER, &ImageWidth, &ImageHeight);
 
-		//threshold pic
-		string img = "..//Data//BackGround//in000160" + ext;
+		//read threshold image
+		string img = "..//Data//Input//in000158" + ext;
 		System::String^ imagePath = marshal_as<System::String^>(img);
-		threshold_img = inputImage(&w, &h, imagePath);
+		thresholdImage = inputImage(&ImageWidth, &ImageHeight, imagePath);
 
-		full_size_of_img = w * h;
-
+		ImageSize = ImageWidth * ImageHeight;
 	}
-	MPI_Bcast(&imagenumber, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&full_size_of_img, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	//send number of images , full size of image, threshold number to all proccesors
+	MPI_Bcast(&IMAGE_NUMBER, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&ImageSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&threshold, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	int** local_image_array = new int* [imagenumber];
-	int* Resultbackgroundimage = new int[full_size_of_img];
-	int* Resultforeground = new int[full_size_of_img];
-	int local_arr_size = (full_size_of_img) / world_size;
-
-	for (int i = 0; i < imagenumber; i++)
+	//Buffer to store image data  inside each proccessor
+	int** localArray = new int* [IMAGE_NUMBER];
+	//store final result of the program
+	int* backgroundAverage = new int[ImageSize];
+	int* foregroundSubtraction = new int[ImageSize];
+	//split the imagesize( columns) into parts for each proccessor
+	int localArraySize = (ImageSize) / world_size;
+	start_s = clock();
+	//for each image
+	for (int i = 0; i < IMAGE_NUMBER; i++)
 	{
+		//master proccessor
 		if (world_rank == 0)
 		{
-
+			//for each image send (first n*size) columns for each n proccessor 
 			for (int j = 1; j < world_size; j++)
 			{
-				MPI_Send(&image_array[i][j * local_arr_size], local_arr_size, MPI_INT, j, 0, MPI_COMM_WORLD);
+				MPI_Send(&TotalImageArray[i][j * localArraySize], localArraySize, MPI_INT, j, 0, MPI_COMM_WORLD);
 			}
 
 			//if processor 0 took all the first parts from all the images start summing them and divide it by the number of images
 			int sum = 0;
-			for (int x = 0; x < local_arr_size; x++) {
-				for (int k = 0; k < imagenumber; k++) {
+			for (int x = 0; x < localArraySize; x++) {
+				for (int k = 0; k < IMAGE_NUMBER; k++) {
 
-					sum += image_array[k][x];
+					sum += TotalImageArray[k][x];
 
 				}
-				sum /= imagenumber;
-				Resultbackgroundimage[x] = sum;
-				int diff = abs(Resultbackgroundimage[x] - threshold_img[x]);
+				sum /= IMAGE_NUMBER;
+				backgroundAverage[x] = sum;
+				int diff = abs(backgroundAverage[x] - thresholdImage[x]);
 				if (diff > threshold)
 				{
-					Resultforeground[x] = diff;
+					foregroundSubtraction[x] = diff;
 				}
 
 				else
 				{
-					Resultforeground[x] = 0;
+					foregroundSubtraction[x] = 0;
 				}
 
 
 			}
-			//if its the last iteration processor 0 will start collecting all the parts of the image from other processors and create it
-			if (i == imagenumber - 1) {
-				/*
-				for (int j = 1; j < world_size; j++)
-				{
-					MPI_Send(&threshold_img[j * local_arr_size], local_arr_size, MPI_INT, j, 0, MPI_COMM_WORLD);
-
-				}
-				*/
+			//final step where master collects image data from other proccesors
+			if (i == IMAGE_NUMBER - 1) {
+				//for each proccessor send threshold image and receive its part of back,forground
 				for (int k = 1; k < world_size; k++)
 				{
 					MPI_Status status;
-					MPI_Send(&threshold_img[k * local_arr_size], local_arr_size, MPI_INT, k, 0, MPI_COMM_WORLD);
-					MPI_Recv(&Resultbackgroundimage[k * local_arr_size], local_arr_size, MPI_INT, k, 0, MPI_COMM_WORLD, &status);
-					MPI_Recv(&Resultforeground[k * local_arr_size], local_arr_size, MPI_INT, k, 0, MPI_COMM_WORLD, &status);
+					MPI_Send(&thresholdImage[k * localArraySize], localArraySize, MPI_INT, k, 0, MPI_COMM_WORLD);
+					MPI_Recv(&backgroundAverage[k * localArraySize], localArraySize, MPI_INT, k, 0, MPI_COMM_WORLD, &status);
+					MPI_Recv(&foregroundSubtraction[k * localArraySize], localArraySize, MPI_INT, k, 0, MPI_COMM_WORLD, &status);
 				}
-
-
-
-				createImage(Resultbackgroundimage, w, h, 0);
-				createImage(Resultforeground, w, h, 1);
-
+				//when done create those images
+				createImage(backgroundAverage, ImageWidth, ImageHeight, 0);
+				createImage(foregroundSubtraction, ImageWidth, ImageHeight, 1);
 			}
-
 		}
 		else if (world_rank <= world_size)
 		{
-
 			//each processor recieve part of the image and save it in local image array
-			local_image_array[i] = new int[local_arr_size];
+			localArray[i] = new int[localArraySize];
 
 			MPI_Status status;
-			MPI_Recv(local_image_array[i], local_arr_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(localArray[i], localArraySize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-
-			//if it recived all the parts of the image it will start working on them
-			if (i == imagenumber - 1)
+			//last iteration has all data start proc
+			if (i == IMAGE_NUMBER - 1)
 			{
-				int* local_threshold = new int[local_arr_size];
-				MPI_Recv(local_threshold, local_arr_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+				//get threshold
+				int* local_threshold = new int[localArraySize];
+				MPI_Recv(local_threshold, localArraySize, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
 				int sum = 0;
-				int* localbackgroundimage = new int[local_arr_size];
-				int* local_forground = new int[local_arr_size];
+				int* localbackgroundimage = new int[localArraySize];
+				int* local_forground = new int[localArraySize];
 
-				for (int x = 0; x < local_arr_size; x++) {
-					for (int k = 0; k < imagenumber; k++) {
+				for (int column = 0; column < localArraySize; column++) {
+					for (int k = 0; k < IMAGE_NUMBER; k++) {
 
-						sum += local_image_array[k][x];
-
-					}
-					sum /= imagenumber;
-					localbackgroundimage[x] = sum;
-					int diff = abs(localbackgroundimage[x] - local_threshold[x]);
-					if (diff > threshold)
-					{
-						local_forground[x] = diff;
+						sum += localArray[k][column];
 
 					}
-					else
-					{
-						local_forground[x] = 0;
-					}
-
-
+					sum /= IMAGE_NUMBER;
+					localbackgroundimage[column] = sum;
+					int diff = abs(localbackgroundimage[column] - local_threshold[column]);
+					if (diff > threshold) { local_forground[column] = diff; }
+					else { local_forground[column] = 0; }
 				}
-				MPI_Send(localbackgroundimage, local_arr_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
-				MPI_Send(local_forground, local_arr_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+				MPI_Send(localbackgroundimage, localArraySize, MPI_INT, 0, 0, MPI_COMM_WORLD);
+				MPI_Send(local_forground, localArraySize, MPI_INT, 0, 0, MPI_COMM_WORLD);
 			}
 		}
 
 	}
 
-
+	stop_s = clock();
+	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
+	cout << "time: " << TotalTime << endl;
 	MPI_Finalize();
 	system("pause");
 	return 0;
-
 }
